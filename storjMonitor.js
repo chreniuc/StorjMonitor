4aa4955e2279165cb1ddf350423249b5627930ce
Kznamst <kznamst@gmail.com>
2017-06-11 01:44:03 +0300

    Create storjMonitor.js

---
+++ storjMonitor.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const dnode = require('dnode');
+const http = require('http');
+const daemon = dnode.connect(45015);
+var token = "Tokenid";
+var options;
+daemon.on('remote', (rpc) => {
+  rpc.status(function(err, shares) {
+    shares.forEach((share) => {
+      options ={
+        host: 'www.storjshare.me',
+        path: '/apiNode.php?token='+token+'&node='+share.id+'&space='+share.meta.farmerState.spaceUsed+'&state='+share.state
+      };
+      http.request(options, function(response) {
+        var str = '';
+        //another chunk of data has been recieved, so append it to `str`
+        response.on('data', function (chunk) {
+          str += chunk;
+        });
+        //the whole response has been recieved, so we just print it out here
+        response.on('end', function () {
+          console.log(str);
+        });
+      }).end();
+      console.log(JSON.stringify(options, null, 4));
+    });
+   daemon.end();
+  });
+});
